9459ada62b4d1244b4d5a68cbb5c9257d4e6dbb1
Varsha <varshabanasode6@gmail.com>
2023-01-18 12:00:49 +0530

    first

---
+++ p4.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+{
+    void main()
+    {
+        int a,b;
+        printf("enter two numbers");
+        scanf("%d%d",&a,&b);
+        if(a>b)
+        printf("the largest number is:%d",a);
+        else
+        printf("the largest number is:%d",b);
+        
+    }
+}
